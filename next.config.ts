9f6c0413e924665347c01b077d4e1d33049a687a
Kunal Kamat <kunalkamat3@gmail.com>
2026-02-22 21:00:40 +0530

    back to experimental

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // experimental: { cacheComponents: true },
+  experimental: { cacheComponents: true },
 };
 
 export default nextConfig;
